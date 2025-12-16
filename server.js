8324ff8d00399f737eaa4b02c41cabcf842a266f
Mudassar Ahmad <mlkmud56@gmail.com>
2025-12-16 09:32:57 +0500

    update cors env

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ALLOWED_ORIGINS,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: false,
